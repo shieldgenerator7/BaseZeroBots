436497ca01e33b1460ea7aed70a92fae12b6c853
shieldgenerator7 <shieldgenerator7@yahoo.com>
2019-05-10 01:04:51 -0400

    Added Shoot instruction

---
+++ Assets/Scripts/InstructionProcesses/Shoot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Shoot", menuName = "Instruction/Shoot", order = 1)]
+public class Shoot : Instruction
+{
+
+    public GameObject projectilePrefab;
+
+    public override void doAction(BotController bc, int currentIndex, List<Instruction> instructions)
+    {
+        GameObject projectile = Instantiate(projectilePrefab);
+        projectile.transform.up = bc.transform.up;
+        projectile.transform.localPosition = bc.transform.localPosition + Vector3.up;
+        projectile.GetComponent<Rigidbody2D>().velocity = Vector2.up;
+    }
+}
---
+++ Assets/Scripts/ShotController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotController : MonoBehaviour
+{
+    public int damage = 1;
+    public float lifetime = 1;
+    public Instruction replaceInstruction;
+
+    private float lifeStartTime;
+
+    private void Start()
+    {
+        lifeStartTime = Time.time;
+    }
+
+    private void Update()
+    {
+        if (Time.time > lifeStartTime + lifetime)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        BotController bc = collision.gameObject.GetComponent<BotController>();
+        if (bc)
+        {
+            for (int i = 0; i < damage; i++)
+            {
+                int randomIndex = Random.Range(0, bc.instructions.Count);
+                bc.instructions[randomIndex] = replaceInstruction;
+            }
+            Destroy(gameObject);
+        }
+    }
+}
