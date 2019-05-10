274fdf6802281fd6aae65b438672755f6443e13f
shieldgenerator7 <shieldgenerator7@yahoo.com>
2019-05-10 03:29:59 -0400

    Uncommitted change from last commit

--- Assets/Scripts/InstructionProcesses/Comparator.cs
+++ Assets/Scripts/InstructionProcesses/Comparator.cs
@@ -5,12 +5,42 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Comparator", menuName = "Instruction/Comparator", order = 1)]
 public class Comparator : Instruction
 {
-    public enum Comparison
+    [Header("Comparators")]
+    public bool lessThan;
+    public bool greaterThan;
+    public bool equalTo;
+
+    public override bool testCondition(BotController bc, int currentIndex, List<Instruction> instructions)
     {
-        LESS_THAN,
-        GREATER_THAN,
-        NEITHER
+        //Can't compare if there's not enough instructions left
+        if (currentIndex >= instructions.Count - 2)
+        {
+            return false;
+        }
+        //Process the comparator
+        int and1 = instructions[currentIndex + 1].instructionToNumber(bc, instructions[currentIndex + 1]);
+        int and2 = instructions[currentIndex + 2].instructionToNumber(bc, instructions[currentIndex + 2]);
+        if (lessThan)
+        {
+            if (and1 < and2)
+            {
+                return true;
+            }
+        }
+        if (greaterThan)
+        {
+            if (and1 > and2)
+            {
+                return true;
+            }
+        }
+        if (equalTo)
+        {
+            if (and1 == and2)
+            {
+                return true;
+            }
+        }
+        return false;
     }
-    public Comparison comparison;
-    public bool orEqualTo;
 }
