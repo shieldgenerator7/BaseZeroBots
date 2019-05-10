c9803fb890e644d0d5c24b6679b137ddd002fda6
shieldgenerator7 <shieldgenerator7@yahoo.com>
2019-05-10 02:40:18 -0400

    Added Rotate instructions

---
+++ Assets/Scripts/InstructionProcesses/Rotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Rotate", menuName = "Instruction/Rotate", order = 1)]
+public class Rotate : Instruction
+{
+    public float angle = 90;
+
+    public override void doAction(BotController bc, int currentIndex, List<Instruction> instructions)
+    {
+        bc.transform.Rotate(-Vector3.forward, angle);
+    }
+}
