9c39bf531b87dfaa5cf53c143200a753469ef3a8
shieldgenerator7 <shieldgenerator7@yahoo.com>
2019-05-15 06:08:09 -0400

    Added Move From Position instruction

--- Assets/Scripts/InstructionProcesses/MoveToPosition.cs
+++ Assets/Scripts/InstructionProcesses/MoveToPosition.cs
@@ -5,11 +5,27 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "MoveToPosition", menuName = "Instruction/MoveToPosition", order = 1)]
 public class MoveToPosition : Instruction
 {
+
+    public enum Option
+    {
+        TOWARDS,
+        AWAY
+    }
+    public Option option;
+
     public override void doAction(BotController bc, int currentIndex, List<Instruction> instructions)
     {
         int param1 = getParameterIndex(1, currentIndex, instructions);
-        Vector2 position = instructions[param1].
+        Vector3 position = instructions[param1].
             instructionToPosition(bc, param1, instructions);
-        bc.moveDirection = position - (Vector2)bc.transform.position;
+        switch (option)
+        {
+            case Option.TOWARDS:
+                bc.moveDirection += (position - bc.transform.position).normalized;
+                break;
+            case Option.AWAY:
+                bc.moveDirection += (bc.transform.position - position).normalized;
+                break;
+        }
     }
 }
