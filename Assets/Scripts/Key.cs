587841ed584f59f667e6ea43c0f6a43d2c751a48
shieldgenerator7 <shieldgenerator7@yahoo.com>
2019-05-22 01:25:35 -0400

    Added key sprites

---
+++ Assets/Scripts/Key.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Key", menuName = "Keys/Key")]
+public class Key : ScriptableObject
+{
+    public KeyCode keyCode;
+    public Sprite keySprite;
+}
---
+++ Assets/Scripts/KeyScheme.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "KeyScheme", menuName = "Keys/KeyScheme")]
+public class KeyScheme : ScriptableObject
+{
+    public int amountOfKeys;
+    public List<Key> keys;
+}
