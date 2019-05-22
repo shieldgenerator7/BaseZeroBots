becd1804e30e1f0ac249b64ef4fae9f453fde3b0
shieldgenerator7 <shieldgenerator7@yahoo.com>
2019-05-22 00:09:14 -0400

    Added AreaTileEditor

---
+++ Assets/Editor/AreaTileEditor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(AreaTile))]
+public class AreaTileEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        if (GUILayout.Button("Update All Visuals"))
+        {
+            ColorScheme cs = FindObjectOfType<GridManager>().colorScheme;
+            foreach(AreaTile at in FindObjectsOfType<AreaTile>())
+            {
+                at.GetComponent<SpriteRenderer>().color = cs.getColor(at.type);
+            }
+        }
+    }
+}
